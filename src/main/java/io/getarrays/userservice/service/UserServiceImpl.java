72ce1b9fd7d115bd4db259f24e0dccc5a4500703
JafarSadikGhub <jafar.sadik@northsouth.edu>
2022-05-18 18:30:39 +0600

    internal

--- src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -6,12 +6,15 @@ import io.getarrays.userservice.repository.RoleRepo;
 import io.getarrays.userservice.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
@@ -47,5 +50,12 @@ public class UserServiceImpl implements UserService{
     public List<AppUser> getUsers() {
         log.info("Fetching all users");
         return userRepo.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepo.findByUsername(username);
+        if()
+        return null;
     }
 }
